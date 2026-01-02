9b0fb0673fa8714bbf035495c40d7e9ab8741dd0
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2026-01-02 12:59:18 +0545

    [ADDED]: added new items in machinery section

--- app/(components)/Machinery.tsx
+++ app/(components)/Machinery.tsx
@@ -8,6 +8,11 @@ import earthCompactor from "../../public/machinery/earth_compactor.png";
 import generator from "../../public/machinery/generator.png";
 import waterPump from "../../public/machinery/water_pump.png";
 import wheelBarrow from "../../public/machinery/wheel_barrow.png";
+import plywood from "../../public/machinery/plywood_formwork.png"
+import steelProps from "../../public/machinery/steel_props.png"
+import steelPipes from "../../public/machinery/steel_pipes.png"
+import clamps from "../../public/machinery/clamps.png"
+import concreteBreaker from "../../public/machinery/concrete_breaker.png"
 
 const Machinery = () => {
   const machineryList = [
@@ -71,10 +76,40 @@ const Machinery = () => {
       quantity: 20,
       image: wheelBarrow,
     },
+    {
+      id: 11,
+      name: "Plywood Formwork",
+      quantity: "4000 sq.m.",
+      image: plywood
+    },
+    {
+      id: 12,
+      name: "Steel Props",
+      quantity: "5000 nos.",
+      image: steelProps
+    },
+    {
+      id: 13,
+      name: "Steel Pipes",
+      quantity: "10000 rm",
+      image: steelPipes
+    },
+    {
+      id: 14,
+      name: "Clamps",
+      quantity: "15000 pcs",
+      image: clamps
+    },
+    {
+      id: 15,
+      name: "Concrete Breaker",
+      quantity: "2 nos.",
+      image: concreteBreaker
+    }
   ];
 
   return (
-    <div className="machinery-list bg-blue-50 py-[5rem]">
+    <div className="machinery-list bg-blue-50 py-[5rem] pb-[8rem]">
       <div className="wrapper">
         <h2 className="header-text text-[2.4rem] md:text-[3.2rem] font-bold mb-[3.5rem] text-center">
           Machinery We Have
@@ -85,9 +120,9 @@ const Machinery = () => {
               <div className="img-container w-[60%] max-h-[15rem] flex items-center justify-center mt-auto">
                 <img src={machine.image.src} alt="machine" className="w-full h-full object-contain" />
               </div>
-              <div className="data flex items-center gap-[1.5rem]">
-                <span className="number text-[2.5rem] font-bold">{machine.quantity}</span>
-                <span className="name text-[1.7rem] font-medium">{machine.name}{machine.quantity > 1 ? "s" : ''}</span>
+              <div className="data flex items-center flex-col gap-[0.5rem]">
+                <span className="name text-[1.7rem] font-medium">{machine.name}</span>
+                <span className="number text-[1.7rem] font-bold">Total: {machine.quantity}</span>
               </div>
             </li>
           ))}
