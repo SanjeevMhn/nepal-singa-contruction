0840e2344e292c6783957c3419d6d40677313a2c
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2026-01-07 16:04:30 +0545

    [ADDED]: added paginated data grid for project list

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ContactUs from "../(components)/ContactUs";
+import DataGrid from "../(components)/DataGrid";
 
 const ProjectPage = async () => {
   const data = await fetch(
@@ -21,50 +22,7 @@ const ProjectPage = async () => {
       </header>
       <div className="project-list-container bg-blue-50">
         <div className="wrapper py-[5rem]">
-          <div className="project-list-table-container bg-white overflow-x-auto py-[2.5rem] rounded-[1.2rem] shadow-xl">
-            <table className="w-full table">
-              <thead>
-                <tr>
-                  <th>S.N</th>
-                  <th>Description</th>
-                  <th>Location</th>
-                  <th>Date</th>
-                  <th>Completed</th>
-                </tr>
-              </thead>
-              <tbody>
-                {projects.map((project: any, index: number) => (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{project.name}</td>
-                    <td>{project.location}</td>
-                    <td>
-                      {project.completed_date !== "" ? (
-                        project.completed_date
-                      ) : (
-                        <p className="text-amber-800 font-medium p-[0.25rem_1.5rem] bg-amber-200 border-2 border-amber-600 rounded-full text-[1.4rem]">
-                          ONGOING
-                        </p>
-                      )}
-                    </td>
-                    <td>
-                      <div className="p-[1.5rem] pl-0">
-                        <div className="progress-bar-container">
-                          <label htmlFor="" className="progress-label">
-                            {project.completed_percent}%
-                          </label>
-                          <progress
-                            max={100}
-                            value={Number(project.completed_percent)}
-                          ></progress>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <DataGrid data={projects} />
         </div>
       </div>
       <ContactUs />
